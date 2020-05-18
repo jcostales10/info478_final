aedc5421b93f3be82f024186bb6163b98f304d41
Yen To <tohoangyennhi@gmail.com>
2020-05-18 16:48:05 -0700

    state county map complete non interactive

--- scripts/districts_by_county.R
+++ scripts/districts_by_county.R
@@ -1,6 +1,14 @@
+#install.packages("maps")
+#install.packages("mapproj")
+#install.packages("plotly")
+#install.packages("rjson")
+
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(plotly)
+library("maps")
+library("mapproj")
 
 data <- read.csv(
   "../data/immunization_2017.csv",
@@ -22,7 +30,11 @@ districts_data <- select(data, -School_District, -County) %>%
   mutate(Immunization_Percentage = (Number_complete_for_all_immunizations/K_12_enrollment) * 100)
 colnames(districts_data)[1] <- "School_District"
 districts_data <- inner_join(districts_data, counties_districts, by = "School_District")
-districts_in_counties <- split(districts_data, districts_data$County)
+districts_in_counties <- split(districts_data, districts_data$County) 
+ 
+districts_data <- rename(districts_data, subregion = County)
+
+
 i <- 1
 plot_list <- list()
 while(i <= length(districts_in_counties)){
@@ -30,6 +42,7 @@ while(i <= length(districts_in_counties)){
   plot_list[[i]] <- df
   i <- i + 1
 }
+
 districts_plot <- lapply(plot_list, function(x){
   ggplot(x) +
     geom_bar(mapping = aes(x = School_District, y = Immunization_Percentage, fill = School_District),
@@ -41,3 +54,46 @@ districts_plot <- lapply(plot_list, function(x){
     facet_wrap(~County)
   }
 )
+
+
+# -------------- map of counties --------------
+map_df <- map_data("county") %>%
+  filter(region == "washington") %>%
+  mutate(subregion = toupper(subregion))
+
+subregion_data <- districts_data %>% 
+  group_by(subregion) %>%
+  summarize(Immunization_Percentage =
+              sum(as.integer(Number_complete_for_all_immunizations))/sum(as.integer(K_12_enrollment))*100,
+            K_12_enrollment = sum(as.integer(K_12_enrollment)),
+            )
+
+county_maps <- left_join(map_df, subregion_data, by = "subregion") 
+
+county_maps$bins <- cut(
+  county_maps$Immunization_Percentage,
+  breaks =c(
+    0, 50, 80, 85, 90, 93, 95, 100
+  ),
+  labels=c(
+    "0-50% ",
+    "50-80% ",
+    "80-85%",
+    "85-90%",
+    "90-93%",
+    "93-95%",
+    "95-100"
+   
+  )
+)
+  
+county_plot <- ggplot(county_maps) + 
+    geom_polygon(aes(x = long, y = lat, group = group, fill = bins)) +
+  scale_fill_brewer(palette = "Blues") +
+  coord_quickmap() +
+  labs(title = " % Immunizations by County 2017", fill = "Immunization Percentage") +
+  theme_void()
+
+
+
+
